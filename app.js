6a9b180577a79acf9fd28bf2239491205d770a69
dipak12803 <dipakrajbhar12803@gmail.com>
2025-10-18 13:08:08 +0530

    Update app.js to use dynamic port for deployment

--- app.js
+++ app.js
@@ -65,6 +65,11 @@ io.on("connection", function (socket) {
     socket.emit("boardState", chess.fen());
 });
 
-server.listen(3000, function () {
-    console.log("listening on port 3000");
+// server.listen(3000, function () {
+//     console.log("listening on port 3000");
+// });
+
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log(`listening on port ${port}`);
 });
